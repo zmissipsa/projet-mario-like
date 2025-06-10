115ce23f2cf636b92e0d69fac2de2d30b61a9d34
itsmissipsa <missipsazouaoui@gmail.com>
2025-06-10 17:52:13 +0200

    [Feature] Ajout de levels au jeu

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 import pygame
 from player import Player
+from level import Level
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1200, 600))
 pygame.display.set_caption("Mario-like")
 clock = pygame.time.Clock()
+
+level1 = Level("niveau1.txt", "tiles.xcf")
 
 # Créer le player
 player = Player(100, 100)
@@ -24,8 +27,11 @@ while running:
     # Update
     player_group.update()
 
+
     # Affichage
     screen.fill((135, 206, 235))  # Bleu ciel
+
+    level1.draw(screen)
     player_group.draw(screen)
 
     pygame.display.flip()
