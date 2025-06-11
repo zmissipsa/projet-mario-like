c7817d6a5e36039e4c9d66459e55c4aab82e8fb2
wissem <wissemamz@gmail.com>
2025-06-11 20:49:11 +0200

    modif locales avant pull

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 from player import Player
 from level import Level
 from sprites import Brick, Coin, Spritesheet
+from enemy import Enemy, Spike
 
 
 pygame.init()
@@ -27,6 +28,25 @@ bricks.add(brick1, brick2)
 
 coins = pygame.sprite.Group()
 
+enemy_spritesheet = Spritesheet("./assets/images/characters.gif")
+enemy1 = Enemy(400, 500, 400, 600, spritesheet=enemy_spritesheet)
+enemy2 = Enemy(700, 500, 700, 900, spritesheet=enemy_spritesheet)
+print(enemy1.image.get_size())  # Doit être (48, 48)
+print(enemy1.rect.topleft)      # Vérifie la position sur l’écran
+
+enemies = pygame.sprite.Group()
+enemies.add(enemy1, enemy2)
+
+# Créer des pics (pièges)
+spikes = pygame.sprite.Group()
+SPIKE_SIZE = 48
+GROUND_Y = 500  # ou la hauteur réelle de la plateforme
+
+spike1 = Spike(1050, GROUND_Y, spritesheet=enemy_spritesheet)
+spike2 = Spike(200, GROUND_Y, spritesheet=enemy_spritesheet)
+spikes.add(spike1, spike2)
+
+
 running = True
 while running:
     clock.tick(60)  # 60 FPS
@@ -42,6 +62,9 @@ while running:
 
     bricks.update()
     coins.update()
+    enemies.update()
+    spikes.update()
+
 
     for brick in bricks:
      if brick.rect.colliderect(player.rect):  # ou une détection précise du saut
@@ -49,7 +72,25 @@ while running:
         if coin:
             coins.add(coin)
 
+ # Collisions avec les ennemis
+    for enemy in enemies:
+        if pygame.sprite.collide_rect(player, enemy) and enemy.alive:
+            if player.vel_y > 0 and player.rect.bottom <= enemy.rect.top + 10:
+                enemy.kill_enemy()
+                player.vel_y = -10  # rebond
+            else:
+                # Joueur meurt
+                player.rect.topleft = (100, 100)
 
+    # Collisions avec les pics
+    # Collision mortelle avec un spike
+    if pygame.sprite.spritecollide(player, spikes, False):
+        font = pygame.font.SysFont("Arial", 72)
+        game_over_text = font.render("GAME OVER", True, (255, 0, 0))
+        screen.blit(game_over_text, (400, 250))
+        pygame.display.flip()
+        pygame.time.wait(2000)  # Pause 2 secondes
+        running = False  # Ferme le jeu
 
     # Affichage
     screen.fill((135, 206, 235))  # Bleu ciel
@@ -58,8 +99,9 @@ while running:
     screen.blit(player.image, player.rect)   # Dessin du joueur
     bricks.draw(screen)
     coins.draw(screen)
-
-
+    enemies.draw(screen)
+    spikes.draw(screen)
+    
     pygame.display.flip()
 
     for platform in plateforms:
